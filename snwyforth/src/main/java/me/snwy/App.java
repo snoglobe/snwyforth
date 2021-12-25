05a2136aa649728f878133e76040ef3eed83ec90
--global <--global>
2021-12-24 20:55:22 -0500

    testing

--- snwyforth/src/main/java/me/snwy/App.java
+++ snwyforth/src/main/java/me/snwy/App.java
@@ -23,8 +23,10 @@ public class App
             String actual = Files.readString(Path.of(args[0]));
             Lexer l = new Lexer(actual);
             l.Lex();
+            System.out.println(l.Tokens);
             Parser p = new Parser(l.Tokens);
             ASTNode AST = p.root();
+            System.out.println(AST);
             Compiler c = new Compiler((Root)AST);
             c.Compile();
             if(verbose)
